32d8df56f16ca10024d695af5d412f32fd4854a0
Faizan Aamer <faizanaryan94@gmail.com>
2022-09-02 15:18:29 +0200

    DIP

--- solid/dependency_inversion/main.go
+++ solid/dependency_inversion/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Dependency Inversion Principle
 
 /* It states that High Level Modules should not depend on Low Level Modules and they both
@@ -17,6 +19,75 @@ type Person struct {
 	Name string
 }
 
-func main() {
+type Info struct {
+	From         *Person
+	Relationship Relationship
+	To           *Person
+}
 
+// low-level module
+type Relationships struct {
+	Relations []Info
+}
+
+func (r *Relationships) AddParentAndChild(parent, child *Person) {
+	r.Relations = append(r.Relations, Info{parent, Parent, child})
+	r.Relations = append(r.Relations, Info{child, Child, parent})
+}
+
+// high-level module
+//here we will be breaking the DIP by making high level module depend on low level module
+
+type Research struct {
+	//Break DIP
+	//Deprecated : ...
+	// relationships Relationships
+
+	//solution property
+	browser RelationshipBrowser
+}
+
+// func (r *Research) Investigate() {
+// 	relations := r.relationships.Relations
+
+// 	for _, rel := range relations {
+// 		if rel.From.Name == "John" && rel.Relationship == Parent {
+// 			fmt.Println("John has a child called : " + rel.To.Name)
+// 		}
+// 	}
+// }
+
+//solution
+type RelationshipBrowser interface {
+	FindAllChildrenOf(name string) []*Person
+}
+
+func (r *Relationships) FindAllChildrenOf(name string) []*Person {
+	result := make([]*Person, 0)
+	for i, person := range r.Relations {
+		if person.Relationship == Parent && person.From.Name == name {
+			result = append(result, r.Relations[i].To)
+		}
+	}
+
+	return result
+}
+
+func (r *Research) InvestigateSolution() {
+	for _, p := range r.browser.FindAllChildrenOf("John") {
+		fmt.Println("John has a child called : " + p.Name)
+	}
+}
+
+func main() {
+	parent := Person{"John"}
+	child1 := Person{"Chris"}
+	child2 := Person{"Matt"}
+
+	relationships := Relationships{}
+	relationships.AddParentAndChild(&parent, &child1)
+	relationships.AddParentAndChild(&parent, &child2)
+
+	research := Research{&relationships}
+	research.InvestigateSolution()
 }
